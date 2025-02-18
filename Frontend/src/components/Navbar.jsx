8798b5e140f9db6c7984d40484395aff88d5cd77
Parineeta  Pareek <parineetapareek215@gmail.com>
2025-02-19 01:17:34 +0530

    Navbar done

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,27 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 function Navbar() {
+  const [sticky, setSticky] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setSticky(true);
+      } else {
+        setSticky(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const navItems = (
+    <>
+      <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>About</a>
+      </li>
+      <li>
+        <a>Contact Us</a>
+      </li>
+    </>
+  );
+
   return (
     <>
-    <aside>
-      <ul>
-        <li>Website Name</li>
-        <li>Menu</li>
-        <ul>
-          <li>Dashboard</li>
-          <li>Transactions</li>
-          <li>My Wallet</li>
-          <li>Goals</li>
-        </ul>
-        <li>Settings</li>
-        <ul>
-          <li>Settings</li>
-          <li>Account</li>
-        </ul>
-      </ul>
-    </aside>
+      <div
+        className={`max-w-screen-2xl container mx-auto md:px-20 px-4 fixed top-0 left-0 right-0 z-50 ${
+          sticky
+            ? "sticky-navbar shadow-md bg-base-200 duration-300 transition-all ease-in-out"
+            : ""
+        }`}
+      >
+        <div className="navbar">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              >
+                {navItems}
+              </ul>
+            </div>
+            <a className="btn btn-ghost text-xl">FinAge</a>
+          </div>
+          <div className="navbar-center hidden lg:flex gap-5">
+            <ul className="menu menu-horizontal px-1">{navItems}</ul>
+          </div>
+          <div className="navbar-end">
+            <button className="btn btn-outline">Sign Up</button>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Navbar;
